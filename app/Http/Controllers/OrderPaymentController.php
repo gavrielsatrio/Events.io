e85cc9eae5471c6dfb60b025649514980df1b8a4
Gavriel Satrio Widjaya <gavriel228@gmail.com>
2023-06-10 17:10:13 +0700

    feat: Add Order Payment Controller

---
+++ app/Http/Controllers/OrderPaymentController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+
+class OrderPaymentController extends Controller
+{
+    public function render(Request $request)
+    {
+        $userID = $request->cookie('user_id');
+        if($userID == null)
+        {
+            return redirect('/');
+        }
+
+        $user = User::find($userID);
+        if($user == null)
+        {
+            Cookie::queue(Cookie::forget('user_id'));
+            return redirect('/');
+        }
+
+        $transactions = Transaction::where('customer_id', $userID)
+            ->where('status', '!=', 'CANCEL')
+            ->orderBy('selected_date', 'desc')
+            ->get();
+
+        return view('order_payment', 
+        [
+            'transactions' => $transactions
+        ]);
+    }
+}
