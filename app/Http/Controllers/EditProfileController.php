08262ae1019a875d8b1fc05ce8e05af3f60e0c22
Gavriel Satrio Widjaya <gavriel228@gmail.com>
2023-06-10 17:06:23 +0700

    feat: Add Edit Profile Controller

---
+++ app/Http/Controllers/EditProfileController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+
+class EditProfileController extends Controller
+{
+    public function render(Request $request)
+    {
+        $userID = $request->cookie('user_id');
+        if($userID == null)
+        {
+            return redirect('/');
+        }
+
+        $user = User::find($userID);
+        if($user == null)
+        {
+            Cookie::queue(Cookie::forget('user_id'));
+            return redirect('/');
+        }
+
+        return view('edit_profile', [
+            'user' => $user
+        ]);
+    }
+
+    public function save(Request $request)
+    {
+        $userID = $request->cookie('user_id');
+
+        User::find($userID)->update([
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'email' => $request->email,
+            'password' => $request->password,
+            'date_of_birth' => $request->date_of_birth,
+            'phone' => $request->phone_number
+        ]);
+
+        return redirect('/account');
+    }
+}
